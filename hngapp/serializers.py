169b7cee004a9bbff7706c81b2f41aa925c7b66f
jaguar48 <okonkwostanley67@yahoo.com>
2022-10-30 03:02:49 -0700

    updateer

--- hngapp/serializers.py
+++ hngapp/serializers.py
@@ -1,13 +1,9 @@
 from rest_framework import serializers
 
 from hngapp.models import hng
-from rest_framework.response import Response
 
 
 class hngSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = hng
         fields = ('slackUsername','backend','age','bio')
-
-def get(self):
-    return Response(hngSerializer)
--- hngapp/views.py
+++ hngapp/views.py
@@ -5,9 +5,21 @@ from rest_framework import viewsets
 
 from .serializers import hngSerializer
 from .models import hng
+from django.http import JsonResponse
 
 
-class hngViewSet(viewsets.ModelViewSet):
-    queryset = hng.objects.all()
-    serializer_class = hngSerializer
-    
+# class hngViewSet(viewsets.ModelViewSet):
+#     queryset = hng.objects.all()
+#     serializer_class = hngSerializer
+#     return serializer_class.json()
+
+
+@api_view(['GET'])
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = hng.objects.all()
+        serializer = hngSerializer(snippets, many=True)
+        return JsonResponse(serializer.data)

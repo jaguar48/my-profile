707aec212ee77aad9dc05faeca505323242d9d9c
jaguar48 <okonkwostanley67@yahoo.com>
2022-10-30 02:48:53 -0700

    update serialzier

--- hngapp/serializers.py
+++ hngapp/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
 from hngapp.models import hng
+from rest_framework.response import Response
+
 
 class hngSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = hng
         fields = ('slackUsername','backend','age','bio')
+
+def get(self):
+    return Response(hngSerializer)

003c9cc6d9f18159153ff421b6331395ae444c41
jaguar48 <okonkwostanley67@yahoo.com>
2022-10-30 17:00:54 -0700

    changes

--- hngapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from hngapp.models import hng
-
-
-class hngSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = hng
-        fields = ('slackUsername','backend','age','bio')
--- hngapp/views.py
+++ hngapp/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 
-from .serializers import hngSerializer
-from .models import hng
+from django.http import JsonResponse
 
-
-class hngViewSet(viewsets.ModelViewSet):
-    queryset = hng.objects.all()
-    serializer_class = hngSerializer
+def myhng(request):
+    my_data = {'slackUsername':'Bishop','backend':True,'age':25,'bio':'I am a programmer'}
+    return JsonResponse(my_data)
     
